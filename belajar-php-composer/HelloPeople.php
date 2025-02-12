8892ed8f6aa3aab9b7eb9597fd38e7c7ec615ec2
Sabilla Muhammad Rayhan <144669955+sabilmr@users.noreply.github.com>
2025-02-12 13:52:39 +0700

    Delete belajar-php-composer directory

--- belajar-php-composer/HelloPeople.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-use Programmerzamannow\BelajarPhpComposer\Data\People;
-
-$people = new People("Sabilla");
-
-echo $people->sayHello("Budi") . PHP_EOL;
